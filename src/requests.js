ef79d2ce7436cf2702261daec1bc8ce0849a72f2
alexanderajju <alexanderajju@gmail.com>
2020-07-10 08:32:50 +0530

    initial with Api call set

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "919f6a07c7da83c7213f5b5629742225";
+
+const requets={
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentarie: `discover/movie?api_key=${API_KEY}&with_genres=28`
+
+
+}
+export default requets;
